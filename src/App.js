8545132ec084d63bf1034716b14b76a93502fd82
Fernando <f.sanchezvilela@mrmilu.com>
2020-10-05 21:44:19 +0100

    add responsive form to our context app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from "./Navbar";
+import Form from "./Form";
 
 class App extends Component {
   render (){
     return(
-      <div className="App">
+      <>
         <Navbar/>
-      </div>
+        <Form/>
+      </>
     );
   }
 }
---
+++ src/Form.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {
+    Avatar,
+    Button,
+    CssBaseline,
+    FormControl,
+    FormControlLabel,
+    Checkbox,
+    Input,
+    InputLabel,
+    Paper,
+    Typography,
+    MenuItem,
+    Select,
+} from '@material-ui/core';
+import { LockOutlined } from "@material-ui/icons";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+import styles from "./styles/FormStyles";
+
+class Form extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <main className={classes.main}>
+               <Paper className={classes.paper}>
+                   <Avatar className={classes.avatar}>
+                       <LockOutlined/>
+                   </Avatar>
+                   <Typography variant="h5">Sign In</Typography>
+                   <Select value='english'>
+                       <MenuItem value="english">English</MenuItem>
+                       <MenuItem value="french">French</MenuItem>
+                       <MenuItem value="spanish">Spanish</MenuItem>
+                   </Select>
+                   <form className={classes.form}>
+                       <FormControl margin="normal" required fullwidth="true">
+                           <InputLabel htmlFor="email">Email</InputLabel>
+                           <Input id="email" autoFocus name="email"></Input>
+                       </FormControl>
+                       <FormControl margin="normal" required fullwidth="true">
+                           <InputLabel htmlFor="password">password</InputLabel>
+                           <Input id="password" autoFocus name="password"></Input>
+                       </FormControl>
+                        <FormControlLabel 
+                            control={<Checkbox color='primary'/>}
+                            label='Remember Me'
+                        />
+                        <Button 
+                            variant="contained" 
+                            type="submit" 
+                            fullwidth='true'
+                            color="primary" 
+                            className={classes.submit}>
+                            SIGN IN
+                        </Button>
+
+                   </form>
+                </Paper>
+            </main>
+        )
+    }
+}
+
+export default withStyles(styles)(Form);
