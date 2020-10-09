b95dc110ae0da5e6cab57da14b8aa71a92e17a08
Fernando <f.sanchezvilela@mrmilu.com>
2020-10-09 17:24:21 +0100

    Updates

---
+++ precache-manifest.d9b5e6418ae1924a692e69b23cb8c52b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e0ec9a3568cefeb4e4673c0db22c44",
+    "url": "/context-class-nav-example/index.html"
+  },
+  {
+    "revision": "d4f891806958e35b9bea",
+    "url": "/context-class-nav-example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "60f1057a43d6269938b8",
+    "url": "/context-class-nav-example/static/js/2.d71fbdeb.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/context-class-nav-example/static/js/2.d71fbdeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f891806958e35b9bea",
+    "url": "/context-class-nav-example/static/js/main.2f8643f4.chunk.js"
+  },
+  {
+    "revision": "b361e0aec7b2b4401531",
+    "url": "/context-class-nav-example/static/js/runtime-main.f958c85b.js"
+  }
+]);
